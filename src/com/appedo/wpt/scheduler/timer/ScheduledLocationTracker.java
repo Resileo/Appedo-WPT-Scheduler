64d7379081350255c0c2c4641db284b0e28fd82d
Prakash-Appedo <prakashg@softsmith.com>
2015-11-18 12:39:38 +0530

    To update and insert locations

--- src/com/appedo/wpt/scheduler/timer/ScheduledLocationTracker.java
+++ src/com/appedo/wpt/scheduler/timer/ScheduledLocationTracker.java
@@ -2,6 +2,8 @@ package com.appedo.wpt.scheduler.timer;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.TimerTask;
 
 import net.sf.json.JSONObject;
@@ -20,20 +22,26 @@ public class ScheduledLocationTracker extends TimerTask {
 	Connection con = null;
 
 	public ScheduledLocationTracker() {
-		
-	}
+		this.con = DataBaseManager.giveConnection();
+		}
 
 	public void run() {
-		con = DataBaseManager.giveConnection();
-		SUMDBI sumdbi = new SUMDBI();
-		HashSet < String > existingloc = sumdbi.extractexistingloc(con);
-		HashSet < String > locToInsert = new HashSet < String > ();
-		// Create Repetitively task for every 1 secs
 		HttpClient client = null;
 		PostMethod method = null;
 		JSONObject joResponse = null;
-
+		SUMDBI sumdbi = null;
+		Set < String > existingAgents = null, allActiveAgents = null,activeDesktopAgents = null,activeMobileAgents = null,
+				desktopAgentsToInsert = null,mobileAgentsToInsert = null;
+		
 		try {
+			allActiveAgents = new HashSet < String > ();
+			activeDesktopAgents = new HashSet < String > ();
+			activeMobileAgents = new HashSet < String > ();
+			desktopAgentsToInsert = new HashSet < String > ();
+			mobileAgentsToInsert = new HashSet < String > ();
+			
+			sumdbi = new SUMDBI();
+			existingAgents = sumdbi.extractexistingloc(con);
 			client = new HttpClient();
 
 			//method = new PostMethod("http://23.23.129.228/getLocations.php");
@@ -43,7 +51,6 @@ public class ScheduledLocationTracker extends TimerTask {
 			int statusCode = client.executeMethod(method);
 			//System.out.println(statusCode);
 			String responseStream = method.getResponseBodyAsString();
-			HashSet < String > activeLocations = new HashSet < String > ();
 
 			if (statusCode == HttpURLConnection.HTTP_OK && responseStream.trim().startsWith("{") && responseStream.trim().endsWith("}")) {
 				StringBuilder keyStrbuildr = new StringBuilder();
@@ -51,47 +58,70 @@ public class ScheduledLocationTracker extends TimerTask {
 				if (!joResponse.getString("data").equals("[]")) {
 					JSONObject locationresp = (JSONObject) joResponse.get("data");
 					for (Object key: locationresp.keySet()) {
-
 						String keyStr = (String) key;
-						activeLocations.add(keyStr.split(":")[0]);
+						if(keyStr.split(":").length==2){
+							activeDesktopAgents.add(keyStr.split(":")[0]);	
+						}else if(keyStr.split(":").length==1){
+							activeMobileAgents.add(keyStr.split(":")[0]);
+						}
+						allActiveAgents.add(keyStr.split(":")[0]);
+					}
+					Iterator<String> itr= allActiveAgents.iterator();
+					while(itr.hasNext()){
+						String keyStr = (String) itr.next();
 						keyStrbuildr.append("'")
 							.append(keyStr.split(":")[0])
 							.append("',");
 					}
 					keyStrbuildr.deleteCharAt(keyStrbuildr.lastIndexOf(","));
 					String activeNodes = keyStrbuildr.toString();
-
-					for (String activeloc: activeLocations) {
-						if (!existingloc.contains(activeloc)) {
-							locToInsert.add(activeloc);
+						//update inactive locations in DB
+						sumdbi.updateInactiveAgents(activeNodes, con);
+						//Desktop
+						for (String activeDeskloc: activeDesktopAgents) {
+							if (!existingAgents.contains(activeDeskloc)) {
+								desktopAgentsToInsert.add(activeDeskloc);
+							}
 						}
-					}
-					// insert new locations
-					if (locToInsert.size() > 0) {
-						sumdbi.insertNewLocation(con, locToInsert);
-					} else {
-						LogManager.infoLog("No Locations to insert");
-					}
-					//update inactive locations
-					sumdbi.updateInactiveLocation(activeNodes, con);
-
+						//mobile
+						for (String activeMobiloc: activeMobileAgents) {
+							if (!existingAgents.contains(activeMobiloc)) {
+								mobileAgentsToInsert.add(activeMobiloc);
+							}
+						}
+						// To insert new Desktop/Mobile Agents
+							if (desktopAgentsToInsert.size() > 0) {
+								sumdbi.insertNewDesktopAgents(con, desktopAgentsToInsert);
+							} else {LogManager.infoLog("No Desktop Locations to insert");}
+							if(mobileAgentsToInsert.size() > 0){
+								sumdbi.insertNewMobileAgents(con, mobileAgentsToInsert);
+							}else{LogManager.infoLog("No Mobile Locations to insert");}
 				} else {
-					sumdbi.updateAllLocationInactive(con);
+					sumdbi.updateAllAgentsInactive(con);
 					LogManager.infoLog("No locations found in getLocations.php API");
-				}
+					}
 			} else {
 				LogManager.infoLog("No response from wpt server. Response status code : "+statusCode);
-			}
-		} catch (Throwable e) {
-			LogManager.errorLog(e);
-			
+				}
+		} catch(Throwable e) {
+			LogManager.errorLog(e);	
+			try {
+				if( ! DataBaseManager.isConnectionExists(con) ){
+					con = DataBaseManager.reEstablishConnection(con);
+				}
+			} catch (SQLException e1) {
+				LogManager.errorLog(e);
+				}
 		}finally{
 			try {
-				if (con != null) {
-					DataBaseManager.close(con);
-				}
-			} catch (Exception e1) {
-				LogManager.errorLog(e1);
+				existingAgents=null;
+				allActiveAgents = null;
+				activeDesktopAgents = null;
+				activeMobileAgents = null;
+				desktopAgentsToInsert=null;
+				mobileAgentsToInsert=null;
+			} catch (Exception e2) {
+				LogManager.errorLog(e2);
 			}
 		}
 	}
